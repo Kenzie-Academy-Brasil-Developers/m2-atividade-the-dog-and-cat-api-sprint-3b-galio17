b83c377ab3c4d579eb852f8554e6aa0a924320dc
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-06-29 11:30:24 -0300

    backup

--- src/script.js
+++ src/script.js
@@ -2,15 +2,14 @@ class PetGenerator {
     static async getPetImage(pet) {
         const petImageContainer = document.getElementById(`${pet}Container`);
         const petImage = document.createElement('img');
-        const image = await fetch(`https://api.the${pet}api.com/v1/images/search`)
-            .then((response) => response.json())
-            .then((response) => response[0].url);
 
-        console.log('carregando');
         petImageContainer.innerHTML = '';
+
         petImage.classList.add('pet__petImage');
         petImage.alt = pet;
-        petImage.src = image;
+        petImage.src = await fetch(`https://api.the${pet}api.com/v1/images/search`)
+            .then((response) => response.json())
+            .then((response) => response[0].url);
 
         petImageContainer.append(petImage);
     }
@@ -27,15 +26,3 @@ class PetGenerator {
 }
 
 PetGenerator.clickEvent();
-
-async function test() {
-    const response = await fetch(`https://api.thedogapi.com/v1/images/search`)
-    const arrayJson = await response.json();
-    const widthDog = arrayJson[0].url;
-
-    const numero = 1;
-
-    console.log(arrayJson)
-}
-
-test()
